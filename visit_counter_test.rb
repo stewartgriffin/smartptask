4c1802830722b434c7eb9315a1ff69ffde5475ef
Tomasz Z <ziajek00@gmail.com>
2019-06-06 21:35:50 +0200

    Add more test cases.

--- visit_counter_test.rb
+++ visit_counter_test.rb
@@ -30,6 +30,25 @@ class VisitCounterTest < Minitest::Test
   end
 
   def test_visit_counter_registers_multiple_paths_from_same_ip
+    2.times do @counter.update("/path", "0.0.0.0") end 
+    3.times do @counter.update("/another_path", "0.0.0.0") end
+    assert_equal 5, @counter.visits
+    assert_equal 2, @counter.unique_visits 
+  end
 
+  def test_visit_counter_same_path_different_ips
+    2.times do @counter.update("/path", "0.0.0.0") end
+    3.times do @counter.update("/path", "1.1.1.1") end
+    assert_equal 5, @counter.visits
+    assert_equal 2, @counter.unique_visits
+  end
+
+  def test_visit_counter_different_ips_different_pahts
+    2.times do @counter.update("/path", "0.0.0.0") end
+    2.times do @counter.update("/path2", "0.0.0.0") end
+    3.times do @counter.update("/path", "1.1.1.1") end
+    4.times do @counter.update("/path", "2.2.2.2") end
+    assert_equal 11, @counter.visits
+    assert_equal 4, @counter.unique_visits
   end
 end
